37b8ab12be7a2224b6a41664a42684a1a4b0235e
afyww <sivinaries@gmail.com>
2025-03-23 07:19:05 +0700

    upp

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\SubTagController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProjectController;
@@ -20,6 +21,10 @@ Route::get('/kategori/{id}', [KategoriController::class, 'show']);
 //TAG
 Route::get('/tags', [TagController::class, 'index']);
 Route::get('/tag/{id}', [TagController::class, 'show']);
+
+//TAG
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/post/{id}', [PostController::class, 'show']);
 
 //PRODUCT
 Route::get('/products', [ProductController::class, 'index']);
